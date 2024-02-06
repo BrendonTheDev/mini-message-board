17e1312a679133ce306f1caff504ca3c21a4a4cf
brendon <108491991+BrendonTheDev@users.noreply.github.com>
2024-02-06 11:31:01 -0500

    install and set ejs

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
+// Set View Engine
+app.set("view engine", "ejs");
+
+// GET and render index
 app.get("/", (req, res) => {
-  res.send("HOME");
+  res.render("index");
 });
 
+// server listen on port 3000
 app.listen(3000);
